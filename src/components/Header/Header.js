bdfa8e1d8c6dace51ca783d0acc19580a8641cd8
José R. Ferreyra <jferreyra.dev@gmail.com>
2022-05-09 21:01:17 -0300

    Estructura inicial del proyecto

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <div className={styles.title}>
+            <h2>Header</h2>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import styles from './Main.module.css';
+
+const Main = () => {
+    return (
+        <div className={styles.title}>
+            <h2>Main</h2>
+        </div>
+    )
+}
+
+export default Main;
